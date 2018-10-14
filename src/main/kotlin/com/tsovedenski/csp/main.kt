801921860b3770bff9db2d854833226855fc7d39
Tsvetan Ovedenski <dev@tsovedenski.com>
2018-10-14 11:39:59 +0200

    Solve SEND+MORE=MONEY

--- src/main/kotlin/com/tsovedenski/csp/main.kt
+++ src/main/kotlin/com/tsovedenski/csp/main.kt
@@ -9,24 +9,37 @@ package com.tsovedenski.csp
  *   = M O N E Y
  */
 fun main(args: Array<String>) {
-    val variables = "SENDMOREMONEY".toList().distinct()
-    val domain = (1..9).toList()
+    val variables = "MSENDMOREMONEY".toList().distinct()
+    val domain = (0..9).toList()
 
-    val constrDplusEeqY: (Solution) -> Boolean = { map ->
-        val d = map.getValue('D')
-        val e = map.getValue('E')
-        val y = map.getValue('Y')
-        d is Selected && e is Selected && y is Selected && d.value + e.value == y.value
+    val `all values are distinct`: Constraint = { map ->
+        val selecteds = map.filter { it.value is Selected }.values as Collection<Selected>
+        val vals = selecteds.map(Selected::value)
+        vals.distinct().size == vals.size
     }
-    val constrTheSum: (Solution) -> Boolean = fn@{ map ->
-        val S = (map.getValue('S') as? Selected)?.value ?: return@fn false
-        val E = (map.getValue('E') as? Selected)?.value ?: return@fn false
-        val N = (map.getValue('N') as? Selected)?.value ?: return@fn false
-        val D = (map.getValue('D') as? Selected)?.value ?: return@fn false
-        val M = (map.getValue('M') as? Selected)?.value ?: return@fn false
-        val O = (map.getValue('O') as? Selected)?.value ?: return@fn false
-        val R = (map.getValue('R') as? Selected)?.value ?: return@fn false
-        val Y = (map.getValue('Y') as? Selected)?.value ?: return@fn false
+
+    val `m is non-zero`: Constraint = fn@{ map ->
+        val M = map.valueOf('M') ?: return@fn false
+        M > 0
+    }
+
+//    example of bad constraint
+//    val `right-most holds`: Constraint = fn@{ map ->
+//        val D = map.valueOf('D') ?: return@fn false
+//        val E = map.valueOf('E') ?: return@fn false
+//        val Y = map.valueOf('Y') ?: return@fn false
+//        D + E == Y
+//    }
+
+    val `whole sum holds`: Constraint = fn@{ map ->
+        val S = map.valueOf('S') ?: return@fn false
+        val E = map.valueOf('E') ?: return@fn false
+        val N = map.valueOf('N') ?: return@fn false
+        val D = map.valueOf('D') ?: return@fn false
+        val M = map.valueOf('M') ?: return@fn false
+        val O = map.valueOf('O') ?: return@fn false
+        val R = map.valueOf('R') ?: return@fn false
+        val Y = map.valueOf('Y') ?: return@fn false
 
         val SEND  =            1000*S + 100*E + 10*N + 1*D
         val MORE  =            1000*M + 100*O + 10*R + 1*E
@@ -35,7 +48,10 @@ fun main(args: Array<String>) {
         SEND + MORE == MONEY
     }
 
-    val task = Task(variables, domain, listOf(constrDplusEeqY, constrTheSum))
+    val task = Task(variables, domain, listOf(
+            `m is non-zero`,
+            `all values are distinct`,
+            `whole sum holds`))
 
     val solution = solve(task)
     println(when (solution?.isSolved()) {
@@ -43,20 +59,32 @@ fun main(args: Array<String>) {
         else -> "FAIL"
     })
 
-    solution?.forEach { c, variable ->
-        println("$c -> $variable")
-    }
+    solution?.print()
 }
+typealias Constraint = (Solution) -> Boolean
 
-typealias Solution = Map<Char, Variable>
+typealias Solution = MutableMap<Char, Variable>
 fun Solution.isSolved(): Boolean = all { it.value is Selected }
+fun Solution.print() = forEach { c, variable -> println("$c -> $variable") }.also { println("-----") }
+fun emptySolution(): Solution = mutableMapOf()
+fun Solution.valueOf(key: Char): Int? = (get(key) as? Selected)?.value
 
-data class Task (val variables: List<Char>, val domain: List<Int>, val constraints: List<(Solution) -> Boolean>) {
-    fun toSolution(): Solution = variables.associate { it to Choice(domain) }
-}
+data class Task (val variables: List<Char>, val domain: List<Int>, val constraints: List<Constraint>)
+fun Task.toSolution(): Solution = variables.associate { it to Choice(domain) }.toMutableMap()
 
-data class Job (val solution: Solution, val constraints: List<(Solution) -> Boolean>) {
-    fun isValid(): Boolean = constraints.all { it(solution) }
+@Suppress("UNCHECKED_CAST")
+data class Job (val solution: Solution, val task: Task, var counter: Long = 0) {
+    fun isValid(): Boolean
+            = task.constraints.all { it(solution) }
+
+    fun isComplete(): Boolean
+            = solution.size == task.variables.size
+
+    fun assignVariable(char: Char, variable: Variable)
+            = apply { solution.apply { set(char, variable) } }
+
+    fun selectUnassignedVariable(): Map.Entry<Char, Choice>?
+            = solution.filterValues { it is Choice }.entries.firstOrNull() as Map.Entry<Char, Choice>?
 }
 
 sealed class Variable
@@ -64,29 +92,26 @@ data class Selected(val value: Int) : Variable()
 data class Choice(val values: List<Int>) : Variable()
 
 fun solve(task: Task): Solution? {
-    val job = Job(task.toSolution(), task.constraints)
-
-    val solution = backtrack(job)
-    return solution
+    val job = Job(task.toSolution(), task)
+    val solved = backtrack(job)
+    solved?.counter?.let { println("Counter = $it") }
+    return solved?.solution
 }
 
-fun backtrack(job: Job): Solution? {
-    if (!job.isValid()) {
-        return null
+fun backtrack(job: Job): Job? {
+    if (job.isValid()) return job
+    job.counter += 1
+    var currentJob = job
+    val variable = job.selectUnassignedVariable() ?: return null
+    variable.value.values.forEach {
+        val attempt = Selected(it)
+        currentJob = currentJob.assignVariable(variable.key, attempt)
+        val result = backtrack(currentJob)
+        if (result != null) {
+            return result
+        }
+        currentJob = currentJob.assignVariable(variable.key, variable.value)
     }
-    if (job.solution.isSolved()) {
-        return job.solution
-    }
 
-    val next = selectValue(job)
-    return backtrack(job)
-}
-
-fun selectValue(job: Job): Job? = job.copy(
-        solution = job.solution.mapValues(::choose)
-)
-
-fun choose(entry: Map.Entry<Char, Variable>): Variable = when (entry.value) {
-    is Selected -> entry.value
-    is Choice   -> Selected((entry.value as Choice).values.first())
+    return null
 }
